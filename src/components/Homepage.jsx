212a51d61d82bf9ffd4ce2e0289a8590ece32dcd
prateek041 <prateeksingh9741@gmail.com>
2022-10-10 01:06:04 +0530

    Basic layout done

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const Homepage = () => {
+  const { Title } = Typography;
+  return (
+    <>
+      <Title level={5}>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total 24h Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Homepage;
